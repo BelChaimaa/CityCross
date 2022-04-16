495f8b8af5a7cc914f998aaf17a048ec816331ff
zumbalove974 <omranedoo@gmail.com>
2022-04-16 19:15:51 +0200

    implémentation du bouton pour récupérer la ville entrée par l'utilisateur

--- app/src/main/java/com/example/myfirstapplication/guidage.java
+++ app/src/main/java/com/example/myfirstapplication/guidage.java
@@ -3,12 +3,41 @@ package com.example.myfirstapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ToggleButton;
+
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
+
+import java.lang.reflect.Array;
 
 public class guidage extends AppCompatActivity {
+
+    private Button btnVille;
+    private TextInputEditText inputVille;
+
+    /* Attributs temporaires utilisés pour les tests en attendant leur implémentation dans les classes dédiées */
+    private float coordsVille[] = {10, 10};
+    private float distance = 100;
+    private float seuilAngle = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_guidage);
+
+        btnVille = findViewById(R.id.btnVille);
+        inputVille = findViewById(R.id.inputVille);
+
+        btnVille.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d("input", inputVille.getText().toString());
+            }
+        });
     }
+
+
 }
