45c36781f32bd9bdb41b2ca9a5443786a89ddd4c
zumbalove974 <omranedoo@gmail.com>
2022-05-05 19:18:55 +0200

    ajout de la bdd ellipsoide, tous semble marcher mais on utilise le srid pour chercher les ellipsoides ce qu'il faudrait modifier

--- app/src/main/java/com/example/myfirstapplication/calage.java
+++ app/src/main/java/com/example/myfirstapplication/calage.java
@@ -1,18 +1,268 @@
 package com.example.myfirstapplication;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Color;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.core.app.ActivityCompat;
+
+import com.google.android.material.textfield.TextInputEditText;
+
+import net.sf.geographiclib.Geodesic;
+import net.sf.geographiclib.GeodesicData;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
+import java.net.URLEncoder;
 
 public class calage extends AppCompatActivity implements Orientation.Listener {
+
+    final int red = Color.parseColor("#F44336");
+    final int green = Color.parseColor("#4CAF50");
+
+    LocationManager locationManager;
+
+    private ConstraintLayout initLayout;
+    private ProgressBar progressBar;
+    private Button btnVille;
+    private TextInputEditText inputVille;
+    private TextView txtDirection;
+    private TextView txtAz1;
+    private TextView txtS12;
+
+    private GuidageView guidageView;
+
+    private Orientation mOrientation;
+    private boolean corrige = false;
+
+    private double[] coordsNordMagnetique = {81.08, -73.13};
+
+    private GeodesicData inverseUtilisateurNordMagnetique;
+    private GeodesicData inverseUtilisateurVille;
+
+    private double[] coordsUtilisateur;
+    private double direction;
+    private String nomVille;
+    private double[] coordsVille;
+    private double distance;
+    private double seuilAngle = 10;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calage);
+
+        locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(calage.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+            return;
+        } else {
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 2000, 10, locationListenerGPS);
+        }
+
+        initLayout = findViewById(R.id.initLayout);
+        progressBar = findViewById(R.id.progressBar);
+        btnVille = findViewById(R.id.btnChercher);
+        inputVille = findViewById(R.id.inputVille);
+        txtDirection = findViewById(R.id.txtDirection);
+        txtAz1 = findViewById(R.id.txtAz1);
+        txtS12 = findViewById(R.id.txtS12);
+
+        mOrientation = new Orientation(this);
+
+        btnVille.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try {
+                    nomVille = inputVille.getText().toString();
+                    calage.bg background = new calage.bg(getApplicationContext());
+                    background.execute(nomVille);
+                    Log.d("input", nomVille);
+                } catch (NullPointerException e) {
+                    Log.d("Error", String.valueOf(e)); // l'appareil n'a pas encore été géolocalisé
+                    progressBar.setVisibility(View.VISIBLE);
+                }
+            }
+        });
     }
 
     @Override
-    public void onOrientationChanged(double azimuth) {
+    protected void onStart() {
+        super.onStart();
+        try {
+            mOrientation.startListening(this);
+        } catch (NullPointerException e){
+            e.printStackTrace();
+        }
 
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        mOrientation.stopListening();
+    }
+
+
+    @Override
+    public void onOrientationChanged(double azimuth) {
+        if(corrige){
+            direction = azimuth + inverseUtilisateurNordMagnetique.azi1;
+            txtDirection.setText(Double.toString(direction));
+            initLayout.removeView(guidageView);
+            guidageView = new GuidageView(this, inverseUtilisateurVille.azi1, direction);
+            initLayout.addView(guidageView);
+            if(Math.abs(direction - inverseUtilisateurVille.azi1) < seuilAngle){
+                initLayout.setBackgroundColor(green);
+            } else {
+                initLayout.setBackgroundColor(red);
+            }
+        } else {
+            txtDirection.setText(Double.toString(azimuth));
+        }
+    }
+
+    LocationListener locationListenerGPS=new LocationListener() {
+        @Override
+        public void onLocationChanged(Location location) {
+            Log.d("display location", "Latitude: "+location.getLatitude()+", longitude: "+location.getLongitude());
+            coordsUtilisateur = new double[]{location.getLatitude(), location.getLongitude()};
+            progressBar.setVisibility(View.INVISIBLE);
+        }
+
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+
+        }
+
+        @Override
+        public void onProviderEnabled(String provider) {
+
+        }
+
+        @Override
+        public void onProviderDisabled(String provider) {
+            Log.d("display location", "location = null");
+            AlertDialog.Builder alertDialog = new AlertDialog.Builder(calage.this);
+
+            alertDialog.setTitle("GPS");
+            alertDialog.setMessage("Votre GPS n'est pas activé, l'application ne peut pas fonctionner sans votre localisation.");
+            alertDialog.setPositiveButton("Paramêtres", new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int which) {
+                    dialog.cancel();
+                    Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                    calage.this.startActivity(intent);
+                }
+            });
+            alertDialog.setNegativeButton("Fermer", new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int which) {
+                    dialog.cancel();
+                    Intent intent = new Intent(calage.this, MainActivity.class);
+                    startActivity(intent);
+                }
+            });
+            alertDialog.show();
+        }
+    };
+
+    private class bg extends AsyncTask<String, Void, double[]> {
+
+        Context c;
+
+        public bg(Context context){
+            this.c = context;
+        }
+
+        @Override
+        protected double[] doInBackground(String... strings) {
+            String result = "";
+            String nomVille = strings[0];
+            String connexion = "http://192.168.1.51/logVille.php";
+            try {
+                URL url = new URL(connexion);
+                HttpURLConnection http = (HttpURLConnection) url.openConnection();
+                http.setRequestMethod("POST");
+                http.setDoInput(true);
+                http.setDoOutput(true);
+                OutputStream ops = http.getOutputStream();
+                BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(ops, "UTF-8"));
+                String data = URLEncoder.encode("nomVille", "UTF-8") + "=" + URLEncoder.encode(nomVille, "UTF-8");
+                writer.write(data);
+                writer.flush();
+                writer.close();
+                InputStream ips = http.getInputStream();
+                BufferedReader reader = new BufferedReader(new InputStreamReader(ips, "UTF-8"));
+                String line;
+                while((line = reader.readLine()) != null){
+                    result += line;
+                }
+                reader.close();
+                ips.close();
+                http.disconnect();
+                JSONArray jArray = new JSONArray(result);
+                String stringCoords = jArray.getJSONObject(0).getString("Coordinates");
+                double latVille = Double.parseDouble(stringCoords.split(",")[0]);
+                double longVille = Double.parseDouble(stringCoords.split(",")[1]);
+                coordsVille = new double[]{latVille, longVille};
+                return coordsVille;
+            } catch (ProtocolException e) {
+                e.printStackTrace();
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (UnsupportedEncodingException e) {
+                e.printStackTrace();
+            } catch (IOException e) { // échec de connexion
+                Log.d("___connexion___", "error");
+                e.printStackTrace();
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            return coordsVille;
+        }
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected void onPostExecute(double[] coordsVille) {
+            super.onPostExecute(coordsVille);
+            inverseUtilisateurVille = Geodesic.WGS84.Inverse(coordsUtilisateur[0], coordsUtilisateur[1], coordsVille[0], coordsVille[1]);
+            inverseUtilisateurNordMagnetique = Geodesic.WGS84.Inverse(coordsUtilisateur[0], coordsUtilisateur[1], coordsNordMagnetique[0], coordsNordMagnetique[1]);
+            corrige = true;
+            txtAz1.setText(inverseUtilisateurVille.azi1 + " ");
+            txtS12.setText(inverseUtilisateurVille.s12 + " ");
+        }
     }
 }
