0f1318ef2aaba18662eed65446b1fc1d54515ac6
zumbalove974 <omranedoo@gmail.com>
2022-05-15 18:19:33 +0200

    modif logEllipsoide.php pour prendre en compte le nom réel des ellipsoides

--- bdd/logEllipsoide.php
+++ bdd/logEllipsoide.php
@@ -6,7 +6,7 @@ $numEllipsoide = $_POST["numEllipsoide"];
 
 $link = new mysqli($servername, $username, $password);
 $tab = [];
-$requete = "SELECT srtext, proj4text FROM citycross.ellipsoides WHERE srtext LIKE '%GEOGCS%\"$numEllipsoide%DATUM%' LIMIT 1";
+$requete = "SELECT srtext, proj4text FROM citycross.ellipsoides WHERE srtext LIKE '%SPHEROID%\"$numEllipsoide%AUTHORITY%' LIMIT 1";
 if ($result = mysqli_query($link, $requete)) {
     while ($ligne = mysqli_fetch_assoc($result)) {
         $tab[] = $ligne;
